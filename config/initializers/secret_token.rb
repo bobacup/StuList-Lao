b3d6ed5d27b53e2e1b847ca4725120a64b32b7f3
Akshay Vangari <akshayvangari@yahoo.co.uk>
2013-11-11 10:16:50 -0800

    configured for psql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StuListApp::Application.config.secret_key_base = '6ba40b8e243c143bb11eca50bbf3f9b3f3ecaf81c310acd86bc2fec1a9f2e70d3d8340cf2c20ace860e0daa9ca23bfaf0e5334bd56e58733392bad37a9665b8a'
