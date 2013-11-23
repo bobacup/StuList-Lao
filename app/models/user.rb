ec164bf93c31fa8e78d2e437f95c7ec63a96f991
Akshay Vangari <akshayvangari@yahoo.co.uk>
2013-11-22 19:55:37 -0800

    added user sign-in feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   before_save { self.username = username.downcase }
   before_save { self.email = email.downcase }
+  before_create :create_remember_token
   validates :first_name,  presence: true, length: { maximum: 50 }
   validates :last_name,  presence: true, length: { maximum: 50 }
   validates :username,  presence: true, length: { minimum: 6 }, uniqueness: { case_sensitive: false }
@@ -10,4 +11,18 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
 end
